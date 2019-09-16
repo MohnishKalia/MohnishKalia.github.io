2a623810813923316f141189c56362168ece9f15
MohnishKalia <20kaliam@elmbrookstudents.org>
2019-09-16 09:59:23 -0500

    CS notes iframe

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+window.onload = () => {
+    for(let elt of document.getElementsByClassName("collapsible"))
+        elt.hidden = true;
+};
+
 document.getElementById("switch").onclick = () => {
     let icon = document.getElementById("favicon");
     if (icon.href === window.location.origin + "/images/joy.png")
         icon.href = window.location.origin + "/images/bird.png";
     else
         icon.href = window.location.origin + "/images/joy.png";
+};
+
+document.getElementById("opener").onclick = () => {
+    for(let elt of document.getElementsByClassName("collapsible"))
+        elt.hidden = false;
 };
