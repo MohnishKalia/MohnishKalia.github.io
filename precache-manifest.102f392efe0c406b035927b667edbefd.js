15bcafbde2ecced7dc1d375462a61a644281ac84
MohnishKalia <mohnish.kalia@gmail.com>
2020-01-02 21:13:01 -0600

    Updates

---
+++ precache-manifest.102f392efe0c406b035927b667edbefd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99a63575730e3fc88745c215529b4f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "384e5d33cb1a8aeaa72d",
+    "url": "/static/js/2.f6c0e1da.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f6c0e1da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd7a1b32c71547e3f373",
+    "url": "/static/js/main.03047947.chunk.js"
+  },
+  {
+    "revision": "f3f5290baee2c8ad6d1a",
+    "url": "/static/js/runtime-main.fec439dd.js"
+  }
+]);
