110a7a50c5ed223f9c521164b2f7311e9eb630ef
MohnishKalia <mohnish.kalia@gmail.com>
2020-01-08 14:25:17 -0600

    Updates

---
+++ precache-manifest.b5939eece011a7c148ad142af6d82829.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d132d6bae5dbce077bcdd3fe536f381f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98e35a7375c0c0d4b3dc",
+    "url": "/static/js/2.e89cc96e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.e89cc96e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6bb7a52131d915aaa229",
+    "url": "/static/js/main.90932d45.chunk.js"
+  },
+  {
+    "revision": "68371729bcd905056a9d",
+    "url": "/static/js/runtime-main.b6df5e3e.js"
+  }
+]);
