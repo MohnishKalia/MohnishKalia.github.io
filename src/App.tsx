d0c2fcefb604b5e216233c7a865eae6dafc445ee
MohnishKalia <mohnish.kalia@gmail.com>
2020-02-21 10:32:52 -0600

    Removed excess and refactored darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import Header from './components/Header'
 import Switch from 'react-switch'
 import Main from './components/Main'
@@ -21,40 +21,41 @@ const App = () => {
 }
 
 export const MainHeader = () =>
-    <nav className="spiers col-md-2 p-0 d-none d-md-block bg-dark sidebar">
-        <div className="sidebar-sticky">
-            <div className="navbar navbar-dark d-flex justify-content-center">
-                <a className="navbar-brand" href="#iam">Mohnish Kalia</a>
+    <nav className="spiers navbar navbar-dark fixed-top col-md-2 d-none d-md-block p-0 vh-100">
+        <div className="navbar navbar-dark d-flex justify-content-center">
+            <a className="navbar-brand" href="#iam">Mohnish Kalia</a>
+        </div>
+        <div className="nav navbar-nav flex-column text-center">
+            <a className="nav-item nav-link" href="#iam">I am...</a>
+            <a className="nav-item nav-link" href="#cards">Past Projects</a>
+            <div className="dropdown">
+                <a className="nav-item nav-link dropdown-toggle" data-toggle="dropdown" href="#/" role="button" aria-haspopup="true" aria-expanded="false">Proficiencies</a>
+                <div className="dropdown-menu">
+                    <a className="dropdown-item" href="#backend">Backend</a>
+                    <a className="dropdown-item" href="#webdevkit">Web Devkit</a>
+                    <a className="dropdown-item" href="#scripting">Scripting</a>
+                    <a className="dropdown-item" href="#softwaredev">Software Development</a>
+                </div>
             </div>
-            <div className="nav flex-column">
-                <a className="nav-link" href="#iam">I am...</a>
-                <a className="nav-link" href="#cards">Past Projects</a>
-                <a className="nav-link" href="#profs">Proficiencies</a>
-                <a className="nav-link" href="#edu">Education</a>
-                <a className="nav-link" href="#footer">Contact</a>
-                <DarkModeForm />
-            </div>
+            <a className="nav-item nav-link" href="#edu">Education</a>
+            <a className="nav-item nav-link" href="#footer">Contact</a>
+            <DarkModeForm />
         </div>
     </nav>
 
 export const DarkModeForm = () => {
     const elt = document.getElementById('theme') as HTMLLinkElement;
 
-    const [theme, setTheme] = useState(matchMedia('(prefers-color-scheme: dark)').matches);
-    const [, setContext] = useContext(ThemeContext);
+    const [context, setContext] = useContext(ThemeContext);
 
-    const changeTheme = (dark: boolean) => { if (elt) elt.href = `https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/${dark ? 'darkly' : 'flatly'}/bootstrap.min.css` }
-
-    changeTheme(theme);
+    useEffect(() => {
+        if (elt) elt.href = `https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/${context ? 'darkly' : 'flatly'}/bootstrap.min.css`
+    }, [context, elt]);
 
     return (
-        <form className="form-inline">
+        <form className="form-inline justify-content-center mt-auto">
             <span className="navbar-text ml-0 ml-sm-3 mr-3 font-weight-bold">Dark Mode</span>
-            <Switch onChange={theme => {
-                setTheme(theme);
-                changeTheme(theme);
-                setContext(theme);
-            }} checked={theme} />
+            <Switch onChange={theme => setContext(theme)} checked={context} />
         </form>
     );
 }
