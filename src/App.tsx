ba2c866595b3b62408736ee3518ee4e7217cd6f2
MohnishKalia <mohnish.kalia@gmail.com>
2020-02-24 14:55:09 -0600

    SimpleBar implementation

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,22 @@ import Main from './components/Main'
 import './App.css';
 import ThemeContext from './ThemeContext'
 import MainHeader from './components/MainHeader'
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
 
 const App = () => {
     const themeHook = useState(matchMedia('(prefers-color-scheme: dark)').matches);
     return (
         <ThemeContext.Provider value={themeHook}>
             <Header />
-            <div className="container-fluid">
-                <div className="row">
-                    <MainHeader />
-                    <Main />
+            <SimpleBar style={{ maxHeight: '100vh' }}>
+                <div className="container-fluid">
+                    <div className="row">
+                        <MainHeader />
+                        <Main />
+                    </div>
                 </div>
-            </div>
+            </SimpleBar>
         </ThemeContext.Provider>
     );
 }
